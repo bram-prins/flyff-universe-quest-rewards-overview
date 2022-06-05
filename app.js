a5433ab5216a97159ec6be9be0077cbe1fc0db40
prins <prins@ciratum.nl>
2022-06-05 03:22:59 +0200

    add port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/', (req, res) => {
 
 // Open server
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
 
