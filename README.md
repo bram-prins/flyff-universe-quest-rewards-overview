41e8138b00b4cec88253f83e2f389f94127cf641
prins <prins@ciratum.nl>
2022-06-05 03:16:49 +0200

    updated readme, and daily check time

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # flyff-universe-quest-rewards-overview
-An overview of the obtainable rewards of the different quests in Flyff Universe
+An overview of the obtainable rewards of the different quests in Flyff Universe.
+This will help you decide which quests to do.
+Information about each quest is linked to flyffipedia.com.
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fetch = require('node-fetch');
-const writeHtml = require('index.js');
+const writeHtml = require('./index.js');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -19,7 +19,7 @@ server.listen(port);
 console.debug('Server listening on port ' + port);
 
 // Get latest game data version and update if needed
-let currentVersion = 6
+let currentVersion = '6'
 const checkGameDataVersion = async () => {
     const gameDataVersion = await (await fetch('https://flyff-api.sniegu.fr/version/data')).json();
     if (currentVersion != gameDataVersion) {
@@ -29,20 +29,20 @@ const checkGameDataVersion = async () => {
 }
 
 // Check game data version every night at 1 am
-const reset = async () => {
+const reset = () => {
     const now = new Date();
-    const night = new Date(
+    const next = new Date(
         now.getFullYear(),
         now.getMonth(),
         now.getDate() + 1, // For the next day, ...
-        3, 0, 0 // ...at 01:00:00 hours
+        1, 0, 0 // ...at 01:00:00 hours
     );
-    const ms = night.getTime() - now.getTime();
+    const ms = next.getTime() - now.getTime();
 
-    setTimeout(() => {
+    setTimeout(async () => {
         await checkGameDataVersion();
         console.debug('Game data version up to date; reset')
-        await reset();
+        reset();
     }, ms);
 }
 reset();
